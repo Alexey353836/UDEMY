39bad00de9b01d0db214e910adf689d98c1993ec
Alexey353836 <example@gmail.com>
2024-01-11 06:36:46 +0200

    tasks- 2

--- index.js
+++ index.js
@@ -251,13 +251,60 @@
 // }
 // console.log(result);
 
-elya: for ( let i = 0; i < 3; i++){
-	console.log(`First level : ${i}`);
-	for (let j = 0; j < 3; j++){
-		console.log(`Second Level: ${j}`);
-		for (let k = 0; k < 5; k++){
-			if(k === 3)break elya;
-			console.log(`Third Level: ${k}`);
-		}
-	}
+// elya: for ( let i = 0; i < 3; i++){
+// 	console.log(`First level : ${i}`);
+// 	for (let j = 0; j < 3; j++){
+// 		console.log(`Second Level: ${j}`);
+// 		for (let k = 0; k < 5; k++){
+// 			if(k === 3)break elya;
+// 			console.log(`Third Level: ${k}`);
+// 		}
+// 	}
+// }
+
+// Место для первой задачи
+function firstTask() {
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++){
+        result[i] = arr[i];
+    }
+    console.log(result);
+    return result;
 }
+firstTask();
+
+// Место для второй задачи
+function secondTask() {
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+    for (let i = 0; i < data.length; i++){
+       if (typeof(data[i]) === 'number'){
+        data[i] = data[i] * 2;
+       }else {
+        if (typeof(data[i]) === 'string'){
+            data[i] = `${data[i]} - done`;
+        }
+       }
+    }
+    console.log(data);
+    return data;
+}
+secondTask();
+
+// Место для третьей задачи
+function thirdTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+
+    // Пишем решение вот тут
+    for (let i = 1; i <= data.length; i++){
+        result[i - 1] = data[data.length - i]
+    }
+    console.log(result);
+    // Не трогаем
+    return result;
+}
+thirdTask();
