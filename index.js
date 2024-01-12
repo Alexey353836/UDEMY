7e1f0dcb4e5eaf988df57f6f34737377eba9e1dc
Alexey353836 <example@gmail.com>
2024-01-12 06:50:00 +0200

    add script.js

--- index.js
+++ index.js
@@ -263,48 +263,61 @@
 // }
 
 // Место для первой задачи
-function firstTask() {
-    const arr = [3, 5, 8, 16, 20, 23, 50];
-    const result = [];
+// function firstTask() {
+//     const arr = [3, 5, 8, 16, 20, 23, 50];
+//     const result = [];
 
-    for (let i = 0; i < arr.length; i++){
-        result[i] = arr[i];
-    }
-    console.log(result);
-    return result;
-}
-firstTask();
+//     for (let i = 0; i < arr.length; i++){
+//         result[i] = arr[i];
+//     }
+//     console.log(result);
+//     return result;
+// }
+// firstTask();
 
 // Место для второй задачи
-function secondTask() {
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
+// function secondTask() {
+//     const data = [5, 10, 'Shopping', 20, 'Homework'];
 
-    for (let i = 0; i < data.length; i++){
-       if (typeof(data[i]) === 'number'){
-        data[i] = data[i] * 2;
-       }else {
-        if (typeof(data[i]) === 'string'){
-            data[i] = `${data[i]} - done`;
-        }
-       }
-    }
-    console.log(data);
-    return data;
-}
-secondTask();
+//     for (let i = 0; i < data.length; i++){
+//        if (typeof(data[i]) === 'number'){
+//         data[i] = data[i] * 2;
+//        }else {
+//         if (typeof(data[i]) === 'string'){
+//             data[i] = `${data[i]} - done`;
+//         }
+//        }
+//     }
+//     console.log(data);
+//     return data;
+// }
+// secondTask();
 
 // Место для третьей задачи
-function thirdTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
-    const result = [];
+// function thirdTask() {
+//     const data = [5, 10, 'Shopping', 20, 'Homework'];
+//     const result = [];
 
-    // Пишем решение вот тут
-    for (let i = 1; i <= data.length; i++){
-        result[i - 1] = data[data.length - i]
-    }
-    console.log(result);
-    // Не трогаем
-    return result;
-}
-thirdTask();
+//     for (let i = 1; i <= data.length; i++){
+//         result[i - 1] = data[data.length - i]
+//     }
+//     console.log(result);
+//     return result;
+// }
+// thirdTask();
+
+
+// const lines = 5;
+// let result = '';
+// // Проверяется именно переменная result, формируйте строку в ней
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+
+// console.log(result)
