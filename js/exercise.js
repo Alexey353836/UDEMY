163fad18a424dd4dea4560f6936c8fb47d8ffea3
Alexey353836 <example@gmail.com>
2024-01-16 23:02:54 +0200

    Execise

--- js/exercise.js
+++ js/exercise.js
@@ -1,1 +1,27 @@
 'use strict';
+
+// Excecise 1 Задание на создание правильных переменных.
+
+// let userName = 'John';
+// let  userNumber = 25;
+//     userNumber = 24;
+
+
+
+
+// Execise 2 Задание на создание правильных типов данных
+
+// const storeName = 'My store';
+// const storeDescription = {
+// 	budget:10000,
+// 	emploes:['Elya', 'Sveta', 'Ira'],
+// 	products: {cofe: 20, tee: 10},
+// 	open:true
+// };
+// console.log(storeName);
+// console.log(storeDescription);
+
+
+
+
+// Execise 3 Задание на использование цыклов и условий
