66a130c7f05dbefdb4d6ed11e91e21c2d2b970c7
Alexey353836 <example@gmail.com>
2024-01-16 19:15:23 +0200

    showOB , writeYourGenres

--- index.js
+++ index.js
@@ -417,6 +417,6 @@
 // const num = 12.2;
 // console.log(Math.round(num));
 
-const test = '12.2px';
-console.log(parseInt(test));
-console.log(parseFloat(test));
+// const test = '12.2px';
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
