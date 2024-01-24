b6472f66bd74dfb4d5ee252cd88bb88a84c485d0
Alexey353836 <example@gmail.com>
2024-01-24 07:52:09 +0200

    Обьекты

--- js/index.js
+++ js/index.js
@@ -425,25 +425,70 @@
 
 // Lesson 31 Callback функции
 
-function first (){
-	setTimeout(function() {
-		console.log(1);
-	},500);
-}
+// function first (){
+// 	setTimeout(function() {
+// 		console.log(1);
+// 	},500);
+// }
 
-function second (){
-	console.log(2);
-}
+// function second (){
+// 	console.log(2);
+// }
 
-first ();
-second ();
+// first ();
+// second ();
 
-function lernJS (lang, calback){
-	console.log(`I lern ${lang}`);
-	calback();
-}
+// function lernJS (lang, calback){
+// 	console.log(`I lern ${lang}`);
+// 	calback();
+// }
 
-function dane(){
-	console.log('YES');
-}
-lernJS('Java Script', dane);
+// function dane(){
+// 	console.log('YES');
+// }
+// lernJS('Java Script', dane);
+
+
+
+
+// lesson - 32 Обьекты, деструкуризация обьектов.
+
+const options = {
+	name: 'test',
+	with: 1024,
+	height: 1024,
+	colors: {
+		border: 'black',
+		bg: 'red'
+	},
+	makeTest: function(){
+		console.log('Test');
+	}
+};
+options.makeTest();
+
+const {border, br} = options.colors;
+console.log(border);
+
+// console.log(Object.keys(options));
+// console.log(Object.keys(options).length);
+
+// console.log(options.colors.border);
+// delete options.name;
+// console.log(options);
+
+// let counter = 0;
+
+// for (let key in options){
+// 	if (typeof (options[key]) === 'object'){
+// 		for (let i in options[key]) {
+// 			console.log(`Свойство ${key} имеет значение ${options[key][i]}`);
+// 			counter++;
+// 		}
+// 	} else {
+// 		console.log(`Свойство ${key} имеет значение ${options[key]}`);
+// 		counter++;
+// 	}
+// }
+// console.log(counter);
+
